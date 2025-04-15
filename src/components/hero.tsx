70bdca06fe6bf11e5a7a308d14fcaeed25974d2a
Sudhansh <96883913+SadhuG@users.noreply.github.com>
2025-04-16 02:21:37 +0530

    hero gradient

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 const Hero = () => {
   return (
-    <section className="bg-cyan-400">
+    <section className="bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90%">
       <div className="flex h-full w-full items-center justify-center bg-[#03030b]/50 p-10">
         <div className="flex w-[70%] flex-col text-center text-white">
           <h1 className="text-8xl font-bold">Enterprise AI Gateway</h1>
